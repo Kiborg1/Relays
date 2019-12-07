d0c7c018d3db411ad84d6ba7cb04d0d3c18f04eb
Kiborg1 <54803738+Kiborg1@users.noreply.github.com>
2019-12-07 12:53:56 +0300

    Update sketch_jun28a.ino

--- sketch_jun28a.ino
+++ sketch_jun28a.ino
@@ -1,4 +1,4 @@
-byte MAP_IN[8]={2,3,4,5,6,7,8,9};
+byte MAP_IN[8]= { 2, 3, 4, 5, 6, 7, 8,9};
 byte MAP_OUT[8]={10,11,12,13,A0,A1,A2,A3};
 
 void setup() {
@@ -13,55 +13,49 @@ Serial.println("STARTED");
 Serial.flush();
 } 
 
-#define ch_max 3 
+#define ch_max 7 
 #define ch_min 0  
 
 int c, ch;
 void loop() {
- while (Serial.available()==0) {};
+ while (Serial.available()<2) {};
  int  buf =   Serial.parseInt();
  c= buf/10;
  ch = buf%10;
+
+ if ((ch>= ch_min) && (ch<= ch_max))
+ {
+    switch (c){
+    case 0://out
+    {  
+      for (int i = ch_min ; i<=ch_max; i++)
+      if (i!=ch)  
+      digitalWrite(  MAP_OUT[ i ], LOW); 
+      delay(100);   
+      digitalWrite(MAP_OUT[ch],HIGH); 
+      break;
+    }
+   case 1: //in
+     {
+      for (int i = ch_min ; i<=ch_max; i++) 
+      if (i!=ch)  
+      digitalWrite(  MAP_IN[i], LOW);  
+      delay(100);   
+      digitalWrite(MAP_IN[ch],HIGH);
+      break;
+     }
+   case 2://off
+      {
+      digitalWrite(  MAP_IN[ch], LOW);
+      digitalWrite(  MAP_OUT[ch], LOW);  
+      delay(100);
+      break;
+      }
+  }
+}
  
  Serial.print(c);
  Serial.print(" ");
  Serial.println(ch); 
-Serial.flush();
- if ((ch>= ch_min) && (ch<= ch_max))
- {
-  if (c == 0 )//out
-  {
-  for (int i = 0 ; i<4; i++)
-    if (i!=ch)  
-      digitalWrite(  MAP_OUT[2*i+1], LOW); 
-   // else
-    //  digitalWrite(  MAP_IN[2*i+1], LOW);
-    delay(100);  
-    for (int i = 0 ;i<4; i++)
-      if (i!=ch)  
-      digitalWrite(  MAP_OUT[2*i], LOW);   
-    //  else
-    //  digitalWrite(  MAP_IN[2*i], LOW);
-  digitalWrite(MAP_OUT[2*ch],HIGH);
-  delay(100) ;
-  digitalWrite(MAP_OUT[2*ch+1],HIGH);
-  }
- if (c == 1 )//in
-  {
-  for (int i = 0 ; i<4; i++)
-    if (i!=ch)  
-      digitalWrite(  MAP_IN[2*i+1], LOW); 
-  //  else
-   //   digitalWrite(  MAP_OUT[2*i+1], LOW);
-    delay(100);  
-  for (int i = 0; i<4; i++)
-      if (i!=ch)  
-      digitalWrite(  MAP_IN[2*i], LOW);   
-  //    else
-  //    digitalWrite(  MAP_OUT[2*i], LOW);
-  digitalWrite(MAP_IN[2*ch],HIGH);
-  delay(100) ;
-  digitalWrite(MAP_IN[2*ch+1],HIGH);
-  }
-}
+Serial.flush();  
 }
