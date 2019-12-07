25db5d4654c1984074b0012a3d505836befe35d5
Kiborg1 <54803738+Kiborg1@users.noreply.github.com>
2019-12-07 10:55:05 +0300

    Update sketch_jun28a.ino

--- sketch_jun28a.ino
+++ sketch_jun28a.ino
@@ -1,12 +1,7 @@
-
 byte MAP_IN[8]={2,3,4,5,6,7,8,9};
-
 byte MAP_OUT[8]={10,11,12,13,A0,A1,A2,A3};
 
-
 void setup() {
-  // put your setup code here, to run once:
-
 DDRB |= B00111111;
 DDRD |= B11111100;
 DDRC |= B00111111;
@@ -15,39 +10,58 @@ PORTD = B00000000;
 PORTC = B00000000;
 Serial.begin(57600);     
 Serial.println("STARTED");
-}
+Serial.flush();
+} 
 
+#define ch_max 3 
+#define ch_min 0  
+
+int c, ch;
 void loop() {
-  // put your main code here, to run repeatedly:
-
  while (Serial.available()==0) {};
  int  buf =   Serial.parseInt();
- int relay_kind  = buf/ 100;
- buf -= relay_kind*100;
- int relay_addr  = buf/ 10;
- int cmd = buf % 10;
+ c= buf/10;
+ ch = buf%10;
  
- Serial.print(relay_kind);
+ Serial.print(c);
  Serial.print(" ");
- Serial.print(relay_addr);
- Serial.print(" ");
- Serial.println(cmd);
- 
-if (cmd == 0 )//off
-{
-if (relay_kind == 0 )//  in
-digitalWrite(MAP_IN[relay_addr], LOW);
-if (relay_kind == 1 )//  out
-digitalWrite(MAP_OUT[relay_addr], LOW);
-delay(100);
+ Serial.println(ch); 
+Serial.flush();
+ if ((ch>= ch_min) && (ch<= ch_max))
+ {
+  if (c == 0 )//out
+  {
+  for (int i = 0 ; i<4; i++)
+    if (i!=ch)  
+      digitalWrite(  MAP_OUT[2*i+1], LOW); 
+   // else
+    //  digitalWrite(  MAP_IN[2*i+1], LOW);
+    delay(100);  
+    for (int i = 0 ;i<4; i++)
+      if (i!=ch)  
+      digitalWrite(  MAP_OUT[2*i], LOW);   
+    //  else
+    //  digitalWrite(  MAP_IN[2*i], LOW);
+  digitalWrite(MAP_OUT[2*ch],HIGH);
+  delay(100) ;
+  digitalWrite(MAP_OUT[2*ch+1],HIGH);
+  }
+ if (c == 1 )//in
+  {
+  for (int i = 0 ; i<4; i++)
+    if (i!=ch)  
+      digitalWrite(  MAP_IN[2*i+1], LOW); 
+  //  else
+   //   digitalWrite(  MAP_OUT[2*i+1], LOW);
+    delay(100);  
+  for (int i = 0; i<4; i++)
+      if (i!=ch)  
+      digitalWrite(  MAP_IN[2*i], LOW);   
+  //    else
+  //    digitalWrite(  MAP_OUT[2*i], LOW);
+  digitalWrite(MAP_IN[2*ch],HIGH);
+  delay(100) ;
+  digitalWrite(MAP_IN[2*ch+1],HIGH);
+  }
 }
-if (cmd == 1 )//on
-{
-if (relay_kind == 0 )//  in
-digitalWrite(MAP_IN[relay_addr], HIGH);
-if (relay_kind == 1 )//  out
-digitalWrite(MAP_OUT[relay_addr], HIGH);
-delay(100);
-}
-
 }
