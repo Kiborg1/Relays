481f6b34af367073c43e62694c5f32264f439709
Kiborg1 <54803738+Kiborg1@users.noreply.github.com>
2019-12-07 10:51:59 +0300

    Update sketch_jun28a.ino

--- sketch_jun28a.ino
+++ sketch_jun28a.ino
@@ -1,152 +1,53 @@
-void blink_13()
-{ 
-PORTB  |=   B00100000;
-delay(50);
-PORTB  &=   B11011111;
-}
+
+byte MAP_IN[8]={2,3,4,5,6,7,8,9};
+
+byte MAP_OUT[8]={10,11,12,13,A0,A1,A2,A3};
 
 
-byte bit_reverse(byte in)
-{
-  byte buf =0;
-  for (byte  i = 0 ; i<8; i++)
-  {
-     buf = buf<<1;
-     buf+= in & B00000001;
-  }
-  return buf;
-}
 void setup() {
-  delay(1000);
   // put your setup code here, to run once:
+
 DDRB |= B00111111;
 DDRD |= B11111100;
 DDRC |= B00111111;
 PORTB = B00000000;
 PORTD = B00000000;
 PORTC = B00000000;
-blink_13();
-Serial.begin(9600);     
-blink_13();
+Serial.begin(57600);     
 Serial.println("STARTED");
 }
 
-
-
 void loop() {
-}
-
-/*
-byte relay_in_status = B00000000 ;
-byte relay_out_status =  B00000000;
-byte no_com = 0;
-byte relay_addr = 0  ;
-byte cmd =0;
-// 0 = off
-// 1 = in
-// 2 = out
-
-void loop() {
-  byte pd;
-  byte pb;
-  byte pc;
-  byte pb1;
   // put your main code here, to run repeatedly:
-relay_addr =(byte) Serial.parseInt();
-cmd = (byte) Serial.parseInt();
-Serial.println("ACCEPTED");
-if ( (0<=relay_addr<8) & (0<=cmd<3))
-  {
-  byte mask =  1<< relay_addr;
-  switch (cmd) 
-  {
-    case 0:
-    mask = ~mask;
-    relay_in_status &= mask;
-    relay_out_status &= mask;
-  
-    pd = relay_in_status ; 
-    pd = pd<< 2 ;
-    pb  = relay_in_status ; 
-    pb = pb>> 6;
-    PORTD  &=pd ;
-    PORTB  &= pb;
 
-    pb = relay_out_status ; 
-    pb = pb<< 2 ;
-    pb&= B00111100;
-    pc  = relay_out_status ; 
-    pc = pc>> 4;
-    pc&= B00001111;
-    PORTB  &=pb ;
-    PORTC  &= pc;
-    break;
-  
-    case 1:
-    relay_out_status &= ~mask;
-    relay_in_status |= mask;
+ while (Serial.available()==0) {};
+ int  buf =   Serial.parseInt();
+ int relay_kind  = buf/ 100;
+ buf -= relay_kind*100;
+ int relay_addr  = buf/ 10;
+ int cmd = buf % 10;
  
-    pd = relay_in_status ; 
-    pd = pd<< 2 ;
-    pb1  = relay_in_status ; 
-    pb1 = pb>> 6; 
-    pb = relay_out_status ; 
-    pb = pb<< 2 ;
-    pb&= B00111100;
-    pb|= pb1;
-    pc  = relay_out_status ; 
-    pc = pc>> 4;
-    pc&= B00001111;
-  
-    PORTD  &=pd ;
-    PORTB  &=pd ;
-    PORTC  &= pc;
-
-    delay(100);
-  
-    PORTD  |=pd ;
-    PORTB  |=pd ;
-    PORTC  |= pc;
-    break;
-  
-    case 2:
-    relay_in_status &= ~mask;
-    relay_out_status |= mask;
-    
-    pd = relay_in_status ; 
-    pd = pd<< 2 ;
-    pb1  = relay_in_status ; 
-    pb1 = pb>> 6; 
-    pb = relay_out_status ; 
-    pb = pb<< 2 ;
-    pb&= B00111100;
-    pb|= pb1;
-    pc  = relay_out_status ; 
-    pc = pc>> 4;
-    pc&= B00001111;
-  
-    PORTD  &=pd ;
-    PORTB  &=pd ;
-    PORTC  &= pc;
-
-    delay(100);
-  
-    PORTD  |=pd ;
-    PORTB  |=pd ;
-    PORTC  |= pc;
-    break;
-
-    delay(100);
-}
-
-
-
-Serial.println("OK");
-}
-else
+ Serial.print(relay_kind);
+ Serial.print(" ");
+ Serial.print(relay_addr);
+ Serial.print(" ");
+ Serial.println(cmd);
+ 
+if (cmd == 0 )//off
 {
-  Serial.println("ERROR");
-  }
+if (relay_kind == 0 )//  in
+digitalWrite(MAP_IN[relay_addr], LOW);
+if (relay_kind == 1 )//  out
+digitalWrite(MAP_OUT[relay_addr], LOW);
+delay(100);
+}
+if (cmd == 1 )//on
+{
+if (relay_kind == 0 )//  in
+digitalWrite(MAP_IN[relay_addr], HIGH);
+if (relay_kind == 1 )//  out
+digitalWrite(MAP_OUT[relay_addr], HIGH);
+delay(100);
 }
 
-*/
+}
